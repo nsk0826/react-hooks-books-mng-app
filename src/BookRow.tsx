cdc5955390f281bbf8d6661876910893865da256
nsk0826 <nsk.20160401@gmail.com>
2022-01-01 02:51:31 +0900

    Bookrow コンポーネントの追加

---
+++ src/BookRow.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { BookToRead } from "./BookToRead";
+
+type BookRowProps = {
+    book: BookToRead,
+    onMemoChange: (id: number, memo: string) => void;
+    onDelete: (id: number) => void;
+};
+
+const BookRow = (props: BookRowProps) => {
+    const { title, authors, memo} = props.book;
+
+    const handleMemoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        props.onMemoChange(props.book.id, e.target.value);
+    }
+
+    const handleDeleteClick = () => {
+        props.onDelete(props.book.id);
+    }
+
+    return (
+        <div className="book-row">
+            <div title={title} className="title">
+                {title}
+            </div>
+            <div title={authors} className="authors">
+                {authors}
+            </div>
+            <input
+                type='text'
+                className="memo"
+                value={memo}
+                onChange={handleMemoChange}
+            />
+            <div className="delete-row" onClick={handleDeleteClick}>
+                削除
+            </div>
+        </div>
+    );
+};
+
+export default BookRow;
