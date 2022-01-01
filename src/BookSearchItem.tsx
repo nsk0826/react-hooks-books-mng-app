b6c2546847dd6d2ffa3105f0eecb9d100c7d24e2
nsk0826 <nsk.20160401@gmail.com>
2022-01-01 22:37:52 +0900

    BookSerchItem コンポーネントの作成

---
+++ src/BookSearchItem.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BookDescription } from "./BookDescription";
+
+type BookSearchItemProps = {
+    description: BookDescription;
+    onBookAdd: (book: BookDescription) => void;
+};
+
+const BookSearchItem = (props: BookSearchItemProps) => {
+    const { title, authors, thumbnail } = props.description;
+    const handleAddBookClick = () => {
+        props.onBookAdd(props.description);
+    };
+
+    return (
+        <div className="book-search-item">
+            <h2 title={title}>{title}</h2>
+            <div className="authors" title={authors}>
+                {authors}
+            </div>
+            {thumbnail ? <img src={thumbnail} alt="" /> : null}
+            <div className="add-book" onClick={handleAddBookClick}>
+                <span>+</span>
+            </div>
+        </div>
+    );
+};
+
+export default BookSearchItem;
